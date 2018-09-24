af32bf4f092f5bb9527de694f41f35fddc3985cf
christophernhill <cnh@mit.edu>
2018-09-24 11:16:50 -0400

    Update Untitled1.jl

--- example1/Untitled1.jl
+++ example1/Untitled1.jl
@@ -11,11 +11,11 @@ pyplot();
 # Note - we put loop inside function so it can operate on Array object and
 # so it can execute efficiently. Calling function for each element of Array
 # is more expensive. 
-ndist(x,μ,σ)=[1/√(2*pi*σ^2)*exp(-((xx-μ)^2)/(2*σ^2)) for xx in x]
+ndist(x,μ,σ)=[1/√(2*π*σ^2)*exp(-((xx-μ)^2)/(2*σ^2)) for xx in x]
 
 # Define a list of values and apply Gaussian function to them, showing timing.
 x=collect(-10:0.1:10);
-μ=0.;σ=pi;@time yy=ndist(x,μ,σ)
+μ=0.;σ=π;@time yy=ndist(x,μ,σ)
 plot(x,yy)
 
 Pkg.add("Statistics")
